49d4fde784dd6703bdbd96f65f385c9d1908a423
Stefanie Taepke <stefanie.taepke@liip.ch>
2017-08-04 17:59:55 +0200

    include fulltext-search and category-filter for events

--- plugins/liip/repaircafe/components/EventList.php
+++ plugins/liip/repaircafe/components/EventList.php
@@ -1,10 +1,14 @@
 <?php namespace Liip\RepairCafe\Components;
 
 use Cms\Classes\ComponentBase;
+use Illuminate\Support\Facades\Input;
+use Liip\RepairCafe\Models\Category;
 use Liip\RepairCafe\Models\Event;
 
 class EventList extends ComponentBase
 {
+    private $categories;
+
     public function componentDetails()
     {
         return [
@@ -16,10 +20,47 @@ class EventList extends ComponentBase
     // This array becomes available on the page as {{ component.eventsGroupedByMonth }}
     public function eventsGroupedByMonth()
     {
-        $events = Event::all();
+        $events = $this->queryEvents();
         return array(
             'August' => $events,
             'September' => $events,
         );
+    }
+
+    public function categories()
+    {
+        return $this->categories;
+    }
+
+    public function onRun()
+    {
+        $this->events = $this->queryEvents();
+        $this->categories = Category::all();
+    }
+
+    protected function queryEvents()
+    {
+        $category = Input::get('category');
+        $searchTerm = Input::get('searchterm');
+
+        if (!empty($searchTerm) && !empty($category)) {
+            $events = Event::whereHas('categories', function ($filter) use ($category) {
+                $filter->where('slug', '=', $category);
+            })
+            ->where(function ($searchFilter) use ($searchTerm) {
+                $searchFilter->where('title', 'like', '%'.$searchTerm.'%')
+                    ->orWhere('description', 'like', '%'.$searchTerm.'%');
+            })->get();
+        } elseif (!empty($searchTerm)) {
+            $events = Event::where('title', 'like', '%'.$searchTerm.'%')
+                ->orWhere('description', 'like', '%'.$searchTerm.'%')->get();
+        } elseif (!empty($category)) {
+            $events = Event::whereHas('categories', function ($filter) use ($category) {
+                $filter->where('slug', '=', $category);
+            })->get();
+        } else {
+            $events = Event::all();
+        }
+        return $events;
     }
 }
