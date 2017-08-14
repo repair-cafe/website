42f463801cf829f5800cc44bac5d22bbba548f15
Juerg Hunziker <juerg.hunziker@liip.ch>
2017-08-14 11:10:55 +0200

    implement bootstrap four pagination presenter

---
+++ plugins/liip/repaircafe/pagination/BootstrapFourUrlWindowPresenterTrait.php
@@ -0,0 +1,64 @@
+<?php namespace Liip\RepairCafe\Pagination;
+
+trait BootstrapFourUrlWindowPresenterTrait
+{
+    /**
+     * Render the actual link slider.
+     *
+     * @return string
+     */
+    protected function getLinks()
+    {
+        $html = '';
+
+        if (is_array($this->window['first'])) {
+            $html .= $this->getUrlLinks($this->window['first']);
+        }
+
+        if (is_array($this->window['slider'])) {
+            $html .= $this->getDots();
+            $html .= $this->getUrlLinks($this->window['slider']);
+        }
+
+        if (is_array($this->window['last'])) {
+            $html .= $this->getDots();
+            $html .= $this->getUrlLinks($this->window['last']);
+        }
+
+        return $html;
+    }
+
+    /**
+     * Get the links for the URLs in the given array.
+     *
+     * @param  array  $urls
+     * @return string
+     */
+    protected function getUrlLinks(array $urls)
+    {
+        $html = '';
+
+        foreach ($urls as $page => $url) {
+            $html .= $this->getPageLinkWrapper($url, $page);
+        }
+
+        return $html;
+    }
+
+    /**
+     * Get HTML wrapper for a page link.
+     *
+     * @param  string  $url
+     * @param  int  $page
+     * @param  string|null  $rel
+     * @return string
+     */
+    protected function getPageLinkWrapper($url, $page, $rel = null)
+    {
+        if ($page == $this->paginator->currentPage()) {
+            return $this->getActivePageWrapper($url, $page);
+        }
+
+        return $this->getAvailablePageWrapper($url, $page, $rel);
+    }
+}
