c7d0bf5724f19475632a9d96f88af101ae4456ca
Juerg Hunziker <juerg.hunziker@liip.ch>
2017-08-16 08:46:27 +0200

    add smooth-scroll implementation

--- themes/repair-cafe/assets/js/app.js
+++ themes/repair-cafe/assets/js/app.js
@@ -2,4 +2,14 @@ $( document ).ready(function() {
     $('#eventSearch select[name="category"]').change(function() {
         this.form.submit();
     });
+
+    // Smooth scrolling when clicking links with smooth-scroll class (requires jquery.easing plugin)
+    $('a.smooth-scroll').bind('click', function(event) {
+        console.log('bla');
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
 });
