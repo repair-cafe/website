8c4496d34edefe3ffb736d6ed91c7688d019e088
Juerg Hunziker <juerg.hunziker@liip.ch>
2017-08-16 19:14:36 +0200

    use mapbox as static image service

--- plugins/liip/repaircafe/config/config.php
+++ plugins/liip/repaircafe/config/config.php
@@ -12,9 +12,8 @@ return [
     |
     */
 
-    'api_key' => 'j9q119t9rGOGfu2WGroSWwLGSfTTTasA',
     'geocoding_api_url' => 'https://maps.googleapis.com/maps/api/geocode/json?address={ADDRESS}&region={REGION}&key={API_KEY}',
-    'static_map_api_url' => 'https://www.mapquestapi.com/staticmap/v5/map?key={API_KEY}&size=540,360&scalebar=false&zoom=17&locations={LATITUDE},{LONGITUDE}',
+    'static_map_api_url' => 'https://api.mapbox.com/styles/v1/mapbox/streets-v10/static/pin-s+4cabe5({LONGITUDE},{LATITUDE})/{LONGITUDE},{LATITUDE},12/540x250@2x?access_token={ACCESS_TOKEN}&logo=false',
     'region' => 'ch',
     'external_map_url' => 'https://www.google.com/maps/search/?api=1&query={QUERY}',
     'events_per_page' => 15,
