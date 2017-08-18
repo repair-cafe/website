811d501f091a56eefe5aa8da71cb9af4334d4e92
Juerg Hunziker <juerg.hunziker@liip.ch>
2017-08-18 10:49:18 +0200

    add property to define max items

--- plugins/liip/repaircafe/components/NewsList.php
+++ plugins/liip/repaircafe/components/NewsList.php
@@ -10,12 +10,26 @@ class NewsList extends ComponentBase
 {
     private $news;
     public $bootstrap_four_presenter;
+    private $max_items;
 
     public function componentDetails()
     {
         return [
             'name' => 'News List',
             'description' => 'Displays a list of news.'
+        ];
+    }
+
+    public function defineProperties()
+    {
+        return [
+            'max_items' => [
+                'title'             => 'Max Items',
+                'description'       => 'Maximum items which should be shown',
+                'type'              => 'string',
+                'validationPattern' => '^[0-9]+$',
+                'validationMessage' => 'The Max Items property can contain only numeric symbols'
+            ],
         ];
     }
 
@@ -26,8 +40,15 @@ class NewsList extends ComponentBase
         setlocale(LC_TIME, $localeCode . '_' . strtoupper($localeCode) . '.UTF-8');
 
         $newsPerPage = Config::get('liip.repaircafe::news_per_page', 9);
-        $news = News::published()->currentLocale()->orderBy('publish_date', 'desc')->paginate($newsPerPage);
-        $this->bootstrap_four_presenter = new BootstrapFourPresenter($news);
+        $this->max_items = $this->property('max_items');
+
+        $newsQuery = News::published()->currentLocale()->orderBy('publish_date', 'desc');
+        if(!empty($this->max_items)) {
+            $news = $newsQuery->take(intval($this->max_items))->get();
+        } else {
+            $news = $newsQuery->paginate($newsPerPage);
+            $this->bootstrap_four_presenter = new BootstrapFourPresenter($news);
+        }
         $this->news = $news;
     }
 
