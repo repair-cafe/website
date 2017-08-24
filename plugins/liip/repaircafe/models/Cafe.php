a87b685491d8eba1e6ffc6465ff311aec9d7a499
Stefanie Taepke <stefanie.taepke@liip.ch>
2017-08-24 10:50:17 +0200

    remove onCreate as this is not required for now

--- plugins/liip/repaircafe/models/Cafe.php
+++ plugins/liip/repaircafe/models/Cafe.php
@@ -95,17 +95,6 @@ class Cafe extends Model
         return $query;
     }
 
-    public function beforeCreate()
-    {
-        /*$user = BackendAuth::getUser();
-
-        if (!$user->isRepairCafeAdmin()) {
-            // TODO: this action still sends a success-message, though it does not create a cafe
-            Flash::error('You are not allowed. Nice try.');
-            return false;
-        }*/
-    }
-
     public function getFormattedAddress()
     {
         $formattedAddress = '';
