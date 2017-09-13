1544ab79073aec931cafb648b205d91a690c6ca7
Juerg Hunziker <juerg.hunziker@liip.ch>
2017-09-13 08:38:21 +0200

    set default values for seo fields

--- plugins/liip/repaircafe/components/NewsDetail.php
+++ plugins/liip/repaircafe/components/NewsDetail.php
@@ -1,7 +1,9 @@
 <?php namespace Liip\RepairCafe\Components;
 
 use Cms\Classes\ComponentBase;
+use Illuminate\Support\Facades\Event;
 use Liip\RepairCafe\Models\News;
+use October\Rain\Support\Facades\Html;
 
 class NewsDetail extends ComponentBase
 {
@@ -32,8 +34,32 @@ class NewsDetail extends ComponentBase
         $this->news = News::published()->where('slug', $this->property('slug'))->first();
         $this->page['news'] = $this->news; // add news object to page context to make seo manager work
         $this->page->title = $this->news->title; // overwrite page title
+
+        // set default seo values if not set
+        Event::listen('seo.beforeComponentRender', function ($page, $seoTag) {
+            if (empty($seoTag->meta_description)) {
+                $seoTag->meta_description = str_limit(Html::strip($this->news->lead), 157);
+            }
+            if (empty($seoTag->og_image)) {
+                $seoTag->og_image = $this->news->image;
+                list($width, $height) = $this->getOgImageDimensions($seoTag->og_image);
+                $seoTag->og_image_width = $width;
+                $seoTag->og_image_height = $height;
+            }
+        });
+
         if (!$this->news) {
             return \Response::make($this->controller->run('404'), 404);
         }
+    }
+
+    protected function getOgImageDimensions($og_image)
+    {
+        $filePath = base_path(config('cms.storage.media.path') . $og_image);
+
+        if (is_file($filePath)) {
+            return getimagesize($filePath);
+        }
+        return false;
     }
 }
